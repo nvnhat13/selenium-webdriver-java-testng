924f897652755efde8b59f0aae26858c81eeac28
nvnhat13 <no130588@gmail.com>
2023-04-26 18:08:17 +0700

    Xpath CSS path 2

---
+++ selenium/javaTester/Topic_02_String.java
@@ -0,0 +1,44 @@
+package javaTester;
+
+public class Topic_02_String {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+boolean a, b;
+// And
+a= true;
+b= true;
+System.out.println(a&&b);
+
+a= true;
+b= false;
+System.out.println(a&&b);
+
+a= false;
+b= false;
+System.out.println(a&&b);
+
+a= false;
+b= true;
+System.out.println(a&&b);
+
+//or
+a= true;
+b= true;
+System.out.println(a || b);
+
+a= true;
+b= false;
+System.out.println(a || b);
+
+a= false;
+b= false;
+System.out.println(a || b);
+
+a= false;
+b= true;
+System.out.println(a || b);
+
+	}
+
+}
