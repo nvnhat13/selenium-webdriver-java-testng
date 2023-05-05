7ee5a509bf7735087dbf97df408f35639f8deb23
nvnhat13 <no130588@gmail.com>
2023-05-05 11:26:53 +0700

    Add topic 06_ Run Browser

--- selenium/webdriver/Topic_06_Run_Browser.java
+++ selenium/webdriver/Topic_06_Run_Browser.java
@@ -6,35 +6,35 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-public class Topic_00_Template {
+public class Topic_06_Run_Browser {
 
    WebDriver driver;
    String projectPath = System.getProperty("user.dir");
    String osName = System.getProperty("os.name");
    @BeforeClass
    public void beforeClass() {
-   if (osName.contains("Windows")){
+	  if (osName.contains("Windows")){
 	   System.setProperty("webdriver.gecko.driver", projectPath + "\\browserDrivers\\geckodriver.exe");
 	   }else {
 		   System.setProperty("webdriver.gecko.driver", projectPath + "/browserDrivers/geckodriver.exe");
 	   }
-	 
+
       driver = new FirefoxDriver();
       driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
       driver.manage().window().maximize();
-	   
+    
    }
 
    @Test
-   public void TC_01_() {
+   public void TC_01_Firefox() {
    }
 
    @Test
-   public void TC_02_() {
+   public void TC_02_Chrome() {
    }
 
    @Test
-   public void TC_03_() {
+   public void TC_03_Edge() {
       
    }
 
