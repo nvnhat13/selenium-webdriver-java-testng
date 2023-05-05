a1fbaf2d10db9fa55f0d7844b72d531fb911da0b
nvnhat13 <no130588@gmail.com>
2023-05-05 17:44:34 +0700

    Topic 07 WebBrowser

---
+++ selenium/javaTester/Topic_04_Debug.java
@@ -0,0 +1,15 @@
+package javaTester;
+
+public class Topic_04_Debug {
+static String homePageUrl;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.print(homePageUrl);
+
+homePageUrl="https://www.youtube.com/watch?v=XSb29GyR-Vc&ab_channel=AutomationFC";
+
+System.out.print(homePageUrl);
+
+	}
+
+}
---
+++ selenium/webdriver/Topic_07_WebBrowser_Commands.java
@@ -0,0 +1,101 @@
+package webdriver;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class Topic_07_WebBrowser_Commands {
+	
+	// Khi viết hàm có thể biết ngay chức năng của hàm đó --> di chuột vào hàm cần kiểm tra
+
+   WebDriver driver;
+   String projectPath = System.getProperty("user.dir");
+   String osName = System.getProperty("os.name");
+   @BeforeClass
+   public void beforeClass() {
+//Windows
+   if (osName.contains("Windows")){
+	   System.setProperty("webdriver.gecko.driver", projectPath + "\\browserDrivers\\geckodriver.exe");
+	   }
+//MacOS
+   else { 		  
+		   System.setProperty("webdriver.gecko.driver", projectPath + "/browserDrivers/geckodriver.exe");
+	   }
+	 
+      driver = new FirefoxDriver();
+      driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+      driver.manage().window().maximize();
+      driver.quit(); //Không đóng tab mà đóng luôn browser     
+   }
+ 
+   @Test
+   public void TC_01_Browser() {
+	   //Các command(hàm) để tương tác với browser thì nó thông báo qua biến driver
+	   //driver. --> có đuôi là WebDriver thì sẽ dùng đc
+	   //Dùng để đóng tab or trình duyệt(nếu chỉ có 1 tab)
+	   driver.close();
+	   //Không đóng tab mà đóng luôn browser  
+	   driver.quit();  
+	   // Tìm 1 element với 1 locator nào đó
+	   driver.findElement(By.id("")); 
+	  // Tìm nhiều element với 1 locator nào đó
+	   driver.findElements(By.xpath("//a[@href")); 
+	   // Mở ra 1 page url nào đó (string)
+	   driver.get("https://www.youtube.com/watch?v=XSb29GyR-Vc&ab_channel=AutomationFC");
+	   // Đang đứng ở page nào thì lấy URL của page đó
+	   driver.getCurrentUrl();
+	   // Cách 1: Dùng cho 1 step
+	   Assert.assertEquals(driver.getCurrentUrl(), "https://www.youtube.com/watch?v=XSb29GyR-Vc&ab_channel=AutomationFC");
+	   // Cách 2: Dùng cho nhiều hơn 1 step thì mới khai báo biến
+	   String homeUrl = driver.getCurrentUrl();
+	   Assert.assertEquals(driver.getCurrentUrl(), "https://www.youtube.com/watch?v=XSb29GyR-Vc&ab_channel=AutomationFC");
+
+	   
+	   
+	   
+	   
+
+   }
+
+   @Test
+   public void TC_02_Element() {
+	   //Các command(hàm) để tương tác với element thì nó thông báo qua việc findElement
+	   //driver.findElement(By.xpath("")); --> vừa là kiểu webDriver, vừa là kiểu findElement
+	   
+	   
+   }
+
+   @Test
+   public void TC_03_Tips() {
+	   // Chia ra gồm 3 nhóm chính
+	   // Nhóm 1: Các hàm để tương tác(action): click, sendKeys, select.... 
+	   //--> tên hàm sẽ thể hiện rõ chức năng của hàm đó mà ko trả về dữ liệu gì hết (ko có return)
+	   driver.findElement(By.xpath("")).click();
+	   
+	   // Nhóm 2: Lấy dữ liệu cho 1 mục đích nào đó 
+	   // --> trả về dữ liệu, đã có kiểu dữ liệu trong suggestion, hầu như là String
+	   // Sẽ bắt đầu bằng tiền tố là getXXX (getText, getTitlte, getAttribute....)
+	   // Dùng để kiểm tra tính đúng đắn của dữ liệu (validate/true/false/equals....) --> đi kèm với hàm Assert(có thư viện riêng trong java)
+	   // Assert: JUnit/TestNG/AssertJ....
+
+	   
+	   // Nhóm 3: Kiểm tra dữ liệu
+	   // Dùng để kiểm tra tính đúng đắn của dữ liệu (true/false/equals) --> hàm assert
+	   // Bắt đầu bằng tiền tố "is": isDisplayed/ isEnable/ isMultiple/... 
+	   // --> trả về dữ liệu (hầu hết là boolean)
+	   
+	   
+      
+   }
+
+   @AfterClass
+   public void afterClass() {
+      driver.quit();
+   }
+
+}
