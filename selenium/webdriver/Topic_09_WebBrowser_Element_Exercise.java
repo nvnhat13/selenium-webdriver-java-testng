6f6d1a41119ac433ba953c1a7231baf0edf093f1
nvnhat13 <no130588@gmail.com>
2023-05-09 11:36:43 +0700

    update TC 09

--- selenium/webdriver/Topic_09_WebBrowser_Element_Exercise.java
+++ selenium/webdriver/Topic_09_WebBrowser_Element_Exercise.java
@@ -36,27 +36,85 @@ public class Topic_09_WebBrowser_Element_Exercise {
 	  
 	   driver.get("http://live.techpanda.org"); 
 	   driver.findElement(By.xpath("//div[@class='footer']//a[text()='My Account']")).click();
-	  Thread.sleep(3000);
+	  Thread.sleep(1000);
 	   Assert.assertEquals(driver.getCurrentUrl(), "http://live.techpanda.org/index.php/customer/account/login/");
 	   driver.findElement(By.xpath("//div[@class='buttons-set']//a[@title='Create an Account']")).click();
-	   Thread.sleep(3000);
+	   Thread.sleep(1000);
 	   Assert.assertEquals(driver.getCurrentUrl(), "http://live.techpanda.org/index.php/customer/account/create/");
 
    }
 
    @Test
    public void TC_02_Title() {
+	   // Vào tab Console, gõ câu lệnh: "document.title" --> hiển thị page title
+	   driver.get("http://live.techpanda.org"); 
+	   driver.findElement(By.xpath("//div[@class='footer']//a[text()='My Account']")).click();
+	  try {
+		Thread.sleep(1000);
+	} catch (InterruptedException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+	   Assert.assertEquals(driver.getTitle(), "Customer Login");
+	   driver.findElement(By.xpath("//div[@class='buttons-set']//a[@title='Create an Account']")).click();
+	   try {
+		Thread.sleep(1000);
+	} catch (InterruptedException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+	   Assert.assertEquals(driver.getTitle(), "Create New Customer Account");
+	   
+	   
    }
 
    @Test
-   public void TC_03_Navigation() {
-      
+   public void TC_03_Navigation() throws InterruptedException {
+	   driver.get("http://live.techpanda.org"); 
+	   driver.findElement(By.xpath("//div[@class='footer']//a[text()='My Account']")).click();
+	   Thread.sleep(1000);
+	  driver.findElement(By.xpath("//div[@class='buttons-set']//a[@title='Create an Account']")).click();
+	   sleepInSecond(1);
+	   Assert.assertEquals(driver.getCurrentUrl(), "http://live.techpanda.org/index.php/customer/account/create/");
+	  driver.navigate().back();
+	  Thread.sleep(1000);
+	  Assert.assertEquals(driver.getCurrentUrl(), "http://live.techpanda.org/index.php/customer/account/login/");
+	  driver.navigate().forward();
+	  sleepInSecond(1);
+	  Assert.assertEquals(driver.getTitle(), "Create New Customer Account");
+	  
    }
    
    @Test
-   public void TC_04_Page_Source() {
-      
+   public void TC_04_Page_Source() throws InterruptedException {
+	   driver.get("http://live.techpanda.org"); 
+	   
+	   driver.findElement(By.xpath("//div[@class='footer']//a[text()='My Account']")).click();
+	   Thread.sleep(3000);
+	  
+	   Assert.assertTrue(driver.getPageSource().contains("Login or Create an Account"));
+
+	  
+	  driver.findElement(By.xpath("//a[@title='Create an Account']")).click();
+	  Thread.sleep(3000);
+	  
+	  Assert.assertTrue(driver.getPageSource().contains("Create an Account"));
+
+	   
    }
+   
+   
+   private void sleepInSecond(long timeout) {
+	   try {
+			Thread.sleep(timeout*1000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+   };
+   
+   
+   
    @AfterClass
    public void afterClass() {
       driver.quit();
