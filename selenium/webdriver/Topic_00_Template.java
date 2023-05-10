97b2f5fe276231b608c955db2545311c3f7732e4
nvnhat13 <no130588@gmail.com>
2023-05-10 17:36:15 +0700

    Update template

--- selenium/webdriver/Topic_00_Template.java
+++ selenium/webdriver/Topic_00_Template.java
@@ -1,4 +1,5 @@
 package webdriver;
+
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
@@ -8,48 +9,55 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+// Các phím tắt trong Eclipse:
+// Duplicate code: bôi tất cả dòng --> Ctrl+Alt+Up
+// Xoá dòng code: Ctrl+D
+// Format code: Ctrl+Shift+F
+// Collapse code: Ctrl+Shift+Down
+// Expand code: Ctrl+Shift+Up
 public class Topic_00_Template {
 
-   WebDriver driver;
-   String projectPath = System.getProperty("user.dir");
-   String osName = System.getProperty("os.name");
-   @BeforeClass
-   public void beforeClass() {
+	WebDriver driver;
+	String projectPath = System.getProperty("user.dir");
+	String osName = System.getProperty("os.name");
+
+	@BeforeClass
+	public void beforeClass() {
 //Windows
-   if (osName.contains("Windows")){
-	   System.setProperty("webdriver.gecko.driver", projectPath + "\\browserDrivers\\geckodriver.exe");
-	   }
+		if (osName.contains("Windows")) {
+			System.setProperty("webdriver.gecko.driver", projectPath + "\\browserDrivers\\geckodriver.exe");
+		}
 //MacOS
-   else { 		  
-		   System.setProperty("webdriver.gecko.driver", projectPath + "/browserDrivers/geckodriver.exe");
-	   }
-	 
-      driver = new FirefoxDriver();
-      driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-      driver.manage().window().maximize();
-      driver.get("http://live.techpanda.org/index.php/customer/account/login/"); 
-   }
+		else {
+			System.setProperty("webdriver.gecko.driver", projectPath + "/browserDrivers/geckodriver.exe");
+		}
 
-   @Test
-   public void TC_01_() {
-	   String benefitText = driver.findElement(By.cssSelector("ul.benefits")).getText();
-	   System.out.println(benefitText)	;  
-   }
+		driver = new FirefoxDriver();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+		driver.get("http://live.techpanda.org/index.php/customer/account/login/");
+	}
 
-   @Test
-   public void TC_02_() {
-   }
+	@Test
+	public void TC_01_() {
+		String benefitText = driver.findElement(By.cssSelector("ul.benefits")).getText();
+		System.out.println(benefitText);
+	}
 
-   @Test
-   public void TC_03_() {
-      
-   }
+	@Test
+	public void TC_02_() {
+	}
 
-   @AfterClass
-   public void afterClass() {
-      driver.quit();
-   }
-   // Ý nghĩa của hàm Assert.assertTrue và Assert.assertFalse
+	@Test
+	public void TC_03_() {
+
+	}
+
+	@AfterClass
+	public void afterClass() {
+		driver.quit();
+	}
+	// Ý nghĩa của hàm Assert.assertTrue và Assert.assertFalse
 //   int a = 5;
 //   int b = 7;
 //   Assert.assertTrue(a < b); // Test sẽ thành công vì 5 < 7
