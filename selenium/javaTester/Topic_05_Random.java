7d6a3ca643eed859c9748b13d1e440aff06e7650
nvnhat13 <no130588@gmail.com>
2023-05-11 18:09:14 +0700

    Update topic 12

--- selenium/javaTester/Topic_05_Random.java
+++ selenium/javaTester/Topic_05_Random.java
@@ -5,8 +5,14 @@ import java.util.Random;
 public class Topic_05_Random {
 	public static void main(String[] args) {
 		Random rand = new Random();
-		String emailAddress = "nhat" + rand.nextInt(9999)+ "gmail.com";
+		String emailAddress = "Anhat" + rand.nextInt(9999)+ "gmail.com";
 		System.out.println(emailAddress);
+		
+		String firstName = "Nhat";
+		String lastName = "Nguyen";
+		String fullName = firstName + " " + lastName;
+		System.out.println(fullName);
+		
 	}
 
 }
--- selenium/webdriver/Topic_12_Textbox_TextArea.java
+++ selenium/webdriver/Topic_12_Textbox_TextArea.java
@@ -1,5 +1,6 @@
 package webdriver;
 
+import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
@@ -21,6 +22,12 @@ public class Topic_12_Textbox_TextArea {
 	WebDriver driver;
 	String projectPath = System.getProperty("user.dir");
 	String osName = System.getProperty("os.name");
+	Random rand = new Random();
+	String emailAddress = "Anhat" + rand.nextInt(9999) + "@gmail.com";
+	String firstName = "Nhat";
+	String lastName = "Nguyen";
+	String fullName = firstName + " " + lastName;
+	String password = "123456789";
 
 	@BeforeClass
 	public void beforeClass() {
@@ -36,30 +43,38 @@ public class Topic_12_Textbox_TextArea {
 		driver = new FirefoxDriver();
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
-		
+
 	}
 
 	@Test
 	public void TC_01_TechPanda_Register() {
-		
+
 		driver.get("http://live.techpanda.org/");
 		sleepInSecond(2);
-	    driver.findElement(By.xpath("//div[@class='footer-container']//a[@title='My Account']")).click();
-	    sleepInSecond(2);
-	    driver.findElement(By.xpath("//div[@class='buttons-set']//a[@title='Create an Account']")).click();
-	    sleepInSecond(2);
-	    driver.findElement(By.xpath("//div[@class='input-box']//input[@id='firstname']")).sendKeys("nguyen1234");
-	    driver.findElement(By.xpath("//div[@class='input-box']//input[@id='lastname']")).sendKeys("nhat123");
-	    driver.findElement(By.xpath("//div[@class='input-box']//input[@id='email_address']")).sendKeys("Nhat1234567@gmail.com");
-	    driver.findElement(By.xpath("//div[@class='input-box']//input[@id='password']")).sendKeys("123aB@");
-	    driver.findElement(By.xpath("//div[@class='input-box']//input[@id='confirmation']")).sendKeys("123aB@");
-	    sleepInSecond(2);
-	    driver.findElement(By.xpath("//div[@class='buttons-set']//button[@title='Register']")).click();
-	    sleepInSecond(3);
-	    Assert.assertTrue(driver.findElement(By.cssSelector("li[class='success-msg']")).isDisplayed());	  
-	    Assert.assertTrue(driver.findElement(By.xpath("//div[@class='box-content']//p[@text()='nguyen1234nhat123']")).isDisplayed());
-	    Assert.assertTrue(driver.findElement(By.xpath("//div[@class='box-content']//br[@text()='Nhat1234567@gmail.com']")).isDisplayed());
-
+		driver.findElement(By.xpath("//div[@class='footer-container']//a[@title='My Account']")).click();
+		sleepInSecond(2);
+		driver.findElement(By.xpath("//div[@class='buttons-set']//a[@title='Create an Account']")).click();
+		sleepInSecond(2);
+		driver.findElement(By.id("firstname")).sendKeys(firstName);
+		driver.findElement(By.id("lastname")).sendKeys(lastName);
+		driver.findElement(By.id("email_address")).sendKeys(emailAddress);
+		driver.findElement(By.id("password")).sendKeys(password);
+		driver.findElement(By.id("confirmation")).sendKeys(password);	
+		
+		driver.findElement(By.xpath("//button[@title='Register']")).click();
+		sleepInSecond(2);
+		Assert.assertEquals(driver.findElement(By.cssSelector("li.success-msg span")).getText(), "Thank you for registering with Main Website Store.");
+		String contactInfor = driver.findElement(By.xpath("//h3[text()='Contact Information']/parent::div/following-sibling::div/p")).getText();
+		System.out.println(contactInfor);
+		
+		Assert.assertTrue(contactInfor.contains(fullName));
+		Assert.assertTrue(contactInfor.contains(emailAddress));
+		driver.findElement(By.xpath("//div[@class='account-cart-wrapper']//a[@class='skip-link skip-account']")).click();
+		sleepInSecond(2);
+		driver.findElement(By.cssSelector("div.skip-content.skip-active a[title='Log Out']")).click();
+		sleepInSecond(2);
+		Assert.assertEquals(driver.getCurrentUrl(),"http://live.techpanda.org/index.php/customer/account/logoutSuccess/");
+		
 	}
 
 	@Test
@@ -70,22 +85,20 @@ public class Topic_12_Textbox_TextArea {
 	public void TC_03_() {
 
 	}
+
 	private void sleepInSecond(long timeout) {
-		   try {
-				Thread.sleep(timeout*1000);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-	   };
+		try {
+			Thread.sleep(timeout * 1000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	};
+
 	@AfterClass
 	public void afterClass() {
 		driver.quit();
 	}
-	// Ý nghĩa của hàm Assert.assertTrue và Assert.assertFalse
-//   int a = 5;
-//   int b = 7;
-//   Assert.assertTrue(a < b); // Test sẽ thành công vì 5 < 7
-//   Assert.assertFalse(a > b); // Test sẽ thành công vì 5 không lớn hơn 7
+
 
 }
