4c2eb12c75e4efd77c18b02d0bfd087b070fcd62
nvnhat13 <no130588@gmail.com>
2023-05-17 14:01:17 +0700

    Add topic 14

--- selenium/webdriver/Topic_14_Custom_Dropdown.java
+++ selenium/webdriver/Topic_14_Custom_Dropdown.java
@@ -1,16 +1,20 @@
 package webdriver;
 
+import java.util.List;
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
-
 
 public class Topic_14_Custom_Dropdown {
 
@@ -18,13 +22,8 @@ public class Topic_14_Custom_Dropdown {
 	String projectPath = System.getProperty("user.dir");
 	String osName = System.getProperty("os.name");
 	Random random = new Random();
-	WebDriverWait explicitlyWait;
-	
-	String emailAddress = "ANhat" + random.nextInt(99999) + "@gmail.com";
-	String firstName = "Nguyen";
-	String lastName = "Nhat";
-	String fullName = firstName + " " + lastName;
-	String password = "123456789";
+	WebDriverWait explicitWait;
+
 	@BeforeClass
 	public void beforeClass() {
 //Windows
@@ -37,45 +36,76 @@ public class Topic_14_Custom_Dropdown {
 		}
 
 		driver = new FirefoxDriver();
-		// Hàm explicitlyWait dùng cho việc kiểm tra trạng thái của element: hiển thị/ko hiển thị/ presence/ staleness
-		explicitlyWait = new WebDriverWait(driver,10);
-		// Hàm implicitlyWait dùng cho việc tìm element (findElement/ findElements)
-		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		explicitWait = new WebDriverWait(driver, 30);
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
-		driver.get("http://live.techpanda.org/index.php/customer/account/login/");
+
 	}
 
 	@Test
-	public void TC_01_() {
-		driver.get("https://www.facebook.com/reg/");
-		// JS code
-		// UnexpectedTagNameException - when element is not selected
-		// Thẻ div, span, ul, li...
-		// Dev tùy biến từ framework họ sử dụng : JQuery/Boostrap/Angular/ReactJS/VueJS...
+	public void TC_01_JQuery() {
+		// Dùng vòng lặp FOR, hàm IF, explicitlyWait, break
+		
+		driver.get("https://jqueryui.com/resources/demos/selectmenu/default.html");
+		
+		selectItemInCustomDropdown("//span[@id='number-button']", "//ul[@id='number-menu']/li/div", "2");		 
+		Assert.assertEquals(driver.findElement(By.xpath("//span[@id='number-button']/span[@class='ui-selectmenu-text']")).getText(), "2");
+		   	
+		selectItemInCustomDropdown("//span[@id='number-button']", "//ul[@id='number-menu']/li/div", "8");		
+		Assert.assertEquals(driver.findElement(By.xpath("//span[@id='number-button']/span[@class='ui-selectmenu-text']")).getText(), "8");
+		
+		selectItemInCustomDropdown("//span[@id='number-button']", "//ul[@id='number-menu']/li/div", "14");
+		Assert.assertEquals(driver.findElement(By.xpath("//span[@id='number-button']/span[@class='ui-selectmenu-text']")).getText(), "14");
+		  
+		selectItemInCustomDropdown("//span[@id='number-button']","//ul[@id='number-menu']/li/div", "18");		
+		Assert.assertEquals(driver.findElement(By.xpath("//span[@id='number-button']/span[@class='ui-selectmenu-text']")).getText(), "18");		  		
 		
 		
+
+		
 	}
 
-	@Test
-	public void TC_02_() {
-	}
-
-	@Test
-	public void TC_03_() {
-
-	}
 	private void sleepInSecond(long timeout) {
-		   try {
-				Thread.sleep(timeout*1000);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-	   };
+		try {
+			Thread.sleep(timeout * 1000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	};
+
+	public void selectItemInCustomDropdown(String xpathParent, String xpathChild, String expectedText) {
+	    // Click vào 1 thẻ để xổ hết các item
+	    driver.findElement(By.xpath(xpathParent)).click();
+	    sleepInSecond(1);
+
+	    // Chờ tất cả item được hiển thị
+	    explicitWait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.xpath(xpathChild)));
+	    // Lấy hết tất cả các item trong dropdown lưu vào list
+	    List<WebElement> allItems = driver.findElements(By.xpath(xpathChild));
+	    // Duyệt qua từng item
+	    for (WebElement tempElement : allItems) {
+	        // Get text của từng item
+	        String itemText = tempElement.getText();
+	        System.out.println(itemText);
+	        // Kiểm tra text đúng với cái mình cần chọn
+	        if (itemText.equals(expectedText)) {
+	            // Scroll tới element cần chọn
+	            ((JavascriptExecutor)driver).executeScript("arguments[0].scrollIntoView(true);", tempElement);	          
+	            sleepInSecond(1);
+	            tempElement.click();
+	            sleepInSecond(1);
+
+	            break;
+	        }
+	    }
+	}
+
+
+	
 	@AfterClass
 	public void afterClass() {
 		driver.quit();
 	}
-
 
 }
