8d97d49321d863f1df2de8d88c8824985c365269
nvnhat13 <no130588@gmail.com>
2023-05-18 17:49:33 +0700

    Add alert authen

--- selenium/webdriver/Topic_16_Alert.java
+++ selenium/webdriver/Topic_16_Alert.java
@@ -4,11 +4,13 @@ import java.util.List;
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
@@ -18,6 +20,7 @@ import org.testng.annotations.Test;
 public class Topic_16_Alert {
 
 	WebDriver driver;
+	
 	String projectPath = System.getProperty("user.dir");
 	String osName = System.getProperty("os.name");
 	Random random = new Random();
@@ -25,6 +28,7 @@ public class Topic_16_Alert {
 	String emailAddress = "ANhat" + random.nextInt(99999) + "@gmail.com";
 	String password = "123456789";
 	JavascriptExecutor jsExecutor;
+	
 	@BeforeClass
 	public void beforeClass() {
 //Windows
@@ -46,15 +50,65 @@ public class Topic_16_Alert {
 		jsExecutor = (JavascriptExecutor) driver;
 	}
 
+	// Alert không inspect được vì nó là của browser
+	// Có 4 loại alert: Accept Alert,  Confirm Alert,  Promt Alert, Authentication Alert
 	// @Test
-	public void TC_01_Alert() {
-		driver.get("https://www.fahasa.com/customer/account/create");
+	public void TC_01_Accept_Alert() {
+//			Alert alert = driver.switchTo().alert();
+//			alert.accept(); = click Ok
+//			alert.dismiss(); = click Cancel
+//			alert.getText(); = Verify
+//			alert.sendKeys(emailAddress); =  nhập nội dung
+		driver.get("https://automationfc.github.io/basic-form/index.html");
+		driver.findElement(By.xpath("//button[text()='Click for JS Alert']")).click();
+		sleepInSecond(1);
+		Alert alert = explicitlyWait.until(ExpectedConditions.alertIsPresent());	
+		Assert.assertEquals(alert.getText(), "I am a JS Alert");
+		alert.accept();
+		Assert.assertEquals(driver.findElement(By.cssSelector("p#result")).getText(),"You clicked an alert successfully");
 
 	}
+	 //@Test
+	public void TC_02_Confirm_Alert() {
+		driver.get("https://automationfc.github.io/basic-form/index.html");
+		driver.findElement(By.xpath("//button[text()='Click for JS Confirm']")).click();
 
-	
-	
-	
+		sleepInSecond(1);
+		Alert alert = explicitlyWait.until(ExpectedConditions.alertIsPresent());	
+		Assert.assertEquals(alert.getText(), "I am a JS Confirm");
+		alert.dismiss();	
+		Assert.assertEquals(driver.findElement(By.cssSelector("p#result")).getText(),"You clicked: Cancel");		
+	}	
+	//@Test
+	public void TC_03_Promt_Alert() {
+		driver.get("https://automationfc.github.io/basic-form/index.html");
+		driver.findElement(By.xpath("//button[text()='Click for JS Prompt']")).click();
+
+		sleepInSecond(1);
+		Alert alert = explicitlyWait.until(ExpectedConditions.alertIsPresent());	
+		Assert.assertEquals(alert.getText(), "I am a JS prompt");
+		alert.sendKeys("Mr Nhat Nguyen");	
+		alert.accept();
+		sleepInSecond(2);
+		Assert.assertEquals(driver.findElement(By.cssSelector("p#result")).getText(),"You entered: Mr Nhat Nguyen");	
+		sleepInSecond(2);
+		
+	}
+		
+	@Test
+	public void TC_04_Authen_Alert() {
+		// Ko dùng thư viện alert để xử lý được
+		// Selenium support: http://username:password@domain
+		driver.get("http://admin:admin@the-internet.herokuapp.com/basic_auth");
+		Assert.assertEquals(driver.findElement(By.cssSelector("div.example p")).getText(), "Congratulations! You must have the proper credentials.");
+		sleepInSecond(2);
+	}
+	@Test
+	public void TC_05_Authen_Alert() {
+		driver.get("http://the-internet.herokuapp.com");
+		
+		
+	}
 
 	private void sleepInSecond(long timeout) {
 		try {
